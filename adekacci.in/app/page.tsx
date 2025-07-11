33eac13d19b1d469c7b53c88d9e2b68da23d6cbf
falahahmed <kpfalah99@gmail.com>
2025-07-11 23:29:39 +0530

    added comment below heading

--- adekacci.in/app/page.tsx
+++ adekacci.in/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="h-screen">
       <div className="grid grid-rows-[20px_1fr_20px] items-center h-[88%] justify-items-center p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <div className={"libertinus-mono-regular text-8xl mb-0"}>Adekacci.<span className="text-primary-green">ORG</span></div>
+        <div>Dreaming of a better world.</div>
         <main className="flex flex-col gap-[32px] mt-0 items-center sm:items-start">
           <div className="grid grid-cols-2 gap-8">
             <Card />
