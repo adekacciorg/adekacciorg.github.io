e73771dbc91f8f55c308682a43106b5e853fa81c
falahahmed <kpfalah99@gmail.com>
2025-08-30 17:55:17 +0530

    added routes. edited some pages

---
+++ adekacci.in/app/about/page.tsx
@@ -0,0 +1,52 @@
+import { Lusitana } from "next/font/google";
+
+const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex flex-col">
+          <Loading />
+          <h2 className="my-12 text-4xl font-semibold text-center text-desert-sand" >
+            <span className={lusitana.className}>
+              {"Hold on, We're working on this!"}
+            </span>
+          </h2>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="grid grid-cols-3 justify-center mx-auto">
+      <div className="w-8 h-8 flex justify-end items-end">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-135 mr-1 animate-load" />
+      </div>
+      <div className="w-8 h-8 flex justify-center items-center">
+        <div className="h-4 w-2 rounded-full bg-light-green animate-load-1" />
+      </div>
+      <div className="w-8 h-8 flex justify-start items-end">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-45 ml-1 animate-load-2" />
+      </div>
+      <div className="w-8 h-8 flex justify-center items-center">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-90 animate-load-7" />
+      </div>
+      <div className="w-8 h-8" />
+      <div className="w-8 h-8 flex justify-center items-center">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-90 animate-load-3" />
+      </div>
+      <div className="w-8 h-8 flex justify-end items-start">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-45 mr-1 animate-load-6" />
+      </div>
+      <div className="w-8 h-8 flex justify-center items-center">
+        <div className="h-4 w-2 rounded-full bg-light-green animate-load-5" />
+      </div>
+      <div className="w-8 h-8 flex justify-start items-start">
+        <div className="h-4 w-2 rounded-full bg-light-green rotate-135 ml-1 animate-load-4" />
+      </div>
+    </div>
+  );
+}
--- adekacci.in/app/page.tsx
+++ adekacci.in/app/page.tsx
@@ -5,29 +5,31 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen">
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <div className={"libertinus-mono-regular text-8xl m-8"}>Adekacci.<span className="text-primary-green">ORG</span></div>
-        <main className="flex flex-col gap-[32px] m-8 p-8 items-center sm:items-start">
-          <div className="grid grid-cols-2 gap-8 mt-8">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center mt-8 justify-items-center p-4 md:p-8 md:mb-[4%] pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="libertinus-mono-regular text-5xl md:text-8xl mt-12 m-4 md:m-8">Adekacci.<span className="text-desert-sand dark:text-light-green">ORG</span></div>
+        <main className="flex flex-col gap-[32px] m-4 md:m-8 p-4 md:p-8 items-center sm:items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 mt-4 md:mt-8">
             <Card
               title="Projects"
-              description="The description about the respective card will be passed from here so that we can test out the options we can use in it"
-              href='/projects'
+              description="You can check out project `downganizer` by clicking this card. We are working on the others."
+              href='/projects/downganizer'
             />
             <Card title="Blogs"
-              description="The description about the respective card will be passed from here so that we can test out the options we can use in it"
+              description="We post various blogs on different tech topics. You can check them out or contribute to them."
+              href='/blogs'
             />
             <Card title="Collaborate"
-              description="The description about the respective card will be passed from here so that we can test out the options we can use in it"
+              description="If you wish to collaborate with us, you can do it in various ways. Click here to know more."
+              href='/collaborate'
             />
             <Card title="About Us"
-              description="The description about the respective card will be passed from here so that we can test out the options we can use in it"
-              href='/'
+              description="We are a group of few developers who would love to do something good. Read about us here."
+              href='/about'
             />
           </div>
         </main>
       </div>
-      <div className="w-full h-px bg-white mb-[2.5%]" />
+      <div className="w-full h-px bg-black dark:bg-white mb-4" />
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center border-white">
       </footer>
     </div>
@@ -37,9 +39,9 @@ export default function Home() {
 function Card({ title, description, href = '/' }: { title?: string, description?: string, href?: Url}) {
   return (
     <Link href={href} >
-      <div className="w-88 rounded-xl p-2 border-white border-1 hover:border-primary-green hover:text-primary-green" >
+      <div className="w-88 h-44 rounded-xl p-2 border-black dark:border-white border-1 hover:border-desert-sand dark:hover:border-light-green hover:text-desert-sand dark:hover:text-light-green" >
         <p className="mt-4 mx-8 text-2xl font-semibold">{title}</p>
-        <p className="text-white/50 mx-8 my-2" >{description}</p>
+        <p className="text-black/50 dark:text-white/50 mx-8 my-2" >{description}</p>
       </div>
     </Link>
   );
